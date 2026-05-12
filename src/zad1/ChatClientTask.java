e8c05146877b9a383bfec835d9130f945064ecdf
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-05-12 20:43:31 +0200

    Changed wait!=0 to wait

--- src/zad1/ChatClientTask.java
+++ src/zad1/ChatClientTask.java
@@ -6,23 +6,22 @@
 
 package zad1;
 
-
 import java.util.List;
 import java.util.concurrent.FutureTask;
 
 public class ChatClientTask extends FutureTask<Void> {
-
     private ChatClient client;
 
     private ChatClientTask(ChatClient c, List<String> msgs, int wait) {
         super(() -> {
             c.login();
-            if (wait != 0) Thread.sleep(wait);
+            if (wait > 0) Thread.sleep(wait);
             for (String m : msgs) {
                 c.send(m);
-                if (wait != 0) Thread.sleep(wait);
+                if (wait > 0) Thread.sleep(wait);
             }
             c.logout();
+            if (wait > 0) Thread.sleep(wait);
             return null;
         });
         this.client = c;
